dcbcee5fb5be14234534d8064e43505847d0329d
mike.mancini <mancini0@gmail.com>
2018-03-07 22:56:03 -0600

    a few more tests

--- contacts-jpa-api/src/test/java/com/mancini/contacts/api/jpa/test/ApiTest.java
+++ contacts-jpa-api/src/test/java/com/mancini/contacts/api/jpa/test/ApiTest.java
@@ -1,5 +1,7 @@
 package com.mancini.contacts.api.jpa.test;
 
+
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +12,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -36,23 +40,77 @@ public class ApiTest {
     }
 
     @Test
-    public void contractsEndpointIs200() throws Exception {
+    public void contractsBaseEndpointFindAll() throws Exception {
         mockMvc.perform(get("/contacts")).andExpect(status().isOk())
-                                                    .andExpect(content().contentType("application/hal+json;charset=UTF-8"));
+                                                    .andExpect(content().contentType("application/hal+json;charset=UTF-8"))
+                                                    .andExpect(jsonPath("$.page.totalElements").value(12));
     }
 
     @Test
-    public void addressesEndpointIs200() throws Exception {
+    public void addressesBaseEndpointFindAll() throws Exception {
         mockMvc.perform(get("/addresses")).andExpect(status().isOk())
-                .andExpect(content().contentType("application/hal+json;charset=UTF-8"));
+                .andExpect(content().contentType("application/hal+json;charset=UTF-8"))
+                .andExpect(jsonPath("$.page.totalElements").value(15));
     }
 
+
+    @Test //Diego Forlan, Diego Milito, Diego Costa, and the G.O.A.T Diego Maradona.
+    public void contractsSimpleQuery() throws Exception {
+        mockMvc.perform(get("/contacts?firstName=Diego")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.page.totalElements").value(4))
+                .andExpect(jsonPath("$.page.totalPages").value(1));
+
+
+    }
+
+
+    @Test//Since there are 4 Diegos, passing a size=1 (to return one result per page) should produce 4 pages.
+    public void paginatedQueryReturnsCorrectPageCount() throws Exception {
+        mockMvc.perform(get("/contacts?firstName=Diego&size=1")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.page.totalElements").value(4))
+                .andExpect(jsonPath("$.page.totalPages").value(4));
+    }
+
+    /**Since I'm using QueryDsl, I should be able to query a Contact through one of its address fields,
+     * accessed via dot notation, (even through address info lives in a seperate table, and is not nested
+     * inside of a Contact entity.
+     *
+     * ie, /contracts?address.city=Salto
+     *
+     * (Both Luis Suarez and Edison Cavani are from Salto, Uruguay)
+     * @throws Exception
+     */
+    @Test
+    public void complexDslQueriesShouldWorkAcrossEntities() throws Exception {
+        mockMvc.perform(get("/contacts?addresses.city=Salto")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.page.totalElements").value(2))
+                .andExpect(jsonPath("$.page.totalPages").value(1))
+                .andExpect(jsonPath("$._embedded.contacts", hasSize(2)))
+                .andExpect((jsonPath("$._embedded.contacts[*].lastName", Matchers.containsInAnyOrder("Suarez","Cavani"))));
+
+        }
+
+
+    /**Since I'm using QueryDsl, I should be able to query a Contact through one of its address fields,
+     * accessed via dot notation, but also filter results on other fields present in the Entity. The above
+     * test shows there are two players from Salto, adding a filter on firstName=Edison should reduce the result set to 1.
+     *
+     * ie, /contracts?firstName=Diego&address.city=Salto
+     *
+     * (Both Luis Suarez and Edison Cavani are from Salto, Uruguay)
+     * @throws Exception
+     */
 
     @Test
-    public void contractsSimpleQuery() throws Exception {
-        mockMvc.perform(get("/contacts")).andExpect(status().isOk())
-                .andExpect(content().contentType("application/hal+json;charset=UTF-8"));
+    public void complexDslQueriesShouldHonorOtherFilters() throws Exception {
+        mockMvc.perform(get("/contacts?addresses.city=Salto&firstName=Edison")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.page.totalElements").value(1))
+                .andExpect(jsonPath("$.page.totalPages").value(1))
+                .andExpect(jsonPath("$._embedded.contacts", hasSize(1)))
+                .andExpect((jsonPath("$._embedded.contacts[*].lastName", Matchers.containsInAnyOrder("Cavani"))));
+
     }
+
 
 
 
